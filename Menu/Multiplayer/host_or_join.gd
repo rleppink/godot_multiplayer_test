31a86dd9e11a07c75822c42330f9650af4f5a6e3
Ronald Leppink <rleppink@Ronalds-MacBook-Air.local>
2023-10-15 13:50:29 +0200

    Update player name config

--- Menu/Multiplayer/host_or_join.gd
+++ Menu/Multiplayer/host_or_join.gd
@@ -4,13 +4,12 @@ extends Control
 func _ready():
 	var config_player_name = ConfigManager.get_value("player_name")
 	if config_player_name:
-		%NameLineEdit.text = config_player_name
+		%PlayerNameLineEdit.text = config_player_name
 		# TODO: Incorrect: this should check sanitized name, but cbf right now
 		_on_sanitized_name_changed(config_player_name)
 
 	var config_player_color = ConfigManager.get_value("player_color")
 	if config_player_color:
-		print("setting color: " + str(config_player_color))
 		%ColorPickerButton.color = config_player_color
 
 
@@ -23,7 +22,6 @@ func _on_sanitized_name_changed(sanitized_name: String):
 		&& regex.search(sanitized_name) == null
 
 	if valid_name:
-		ConfigManager.set_value("player_name", sanitized_name)
 		%HostButton.disabled = false
 		%JoinButton.disabled = false
 	else:
@@ -36,12 +34,12 @@ func _on_back_button_pressed():
 
 
 func _on_join_button_pressed():
+	ConfigManager.set_value("player_name", %PlayerNameLineEdit.text)
+	ConfigManager.set_value("player_color", %ColorPickerButton.color)
 	get_tree().change_scene_to_file("res://Menu/Multiplayer/Join/join_game.tscn")
 
 
 func _on_host_button_pressed():
-	get_tree().change_scene_to_file("res://Menu/Multiplayer/Host/host_game.tscn")
-
-
-func _on_color_picker_button_color_changed(color:Color):
+	ConfigManager.set_value("player_name", %PlayerNameLineEdit.text)
 	ConfigManager.set_value("player_color", %ColorPickerButton.color)
+	get_tree().change_scene_to_file("res://Menu/Multiplayer/Host/host_game.tscn")
