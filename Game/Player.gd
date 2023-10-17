43dd49c3830c8447ed661572aa5704b32054c103
Ronald Leppink <rleppink@Ronalds-MacBook-Air.local>
2023-10-17 11:57:11 +0200

    Move movement code to Player script

--- Game/Player.gd
+++ Game/Player.gd
@@ -8,6 +8,7 @@ const SPEED = 500.0
 @export var velocity_sync: Vector2
 @export var color: Color
 
+
 func _ready():
 	$NameLabel.text = player_name
 	$AnimatedSprite2D.modulate = Color(
@@ -16,7 +17,10 @@ func _ready():
 		0.8 + clamp(color.b, 0.0, 0.5),
 		255)
 
+
 func _process(_delta: float) -> void:
+	process_input()
+
 	z_index = int(round(position.y))
 	
 	if velocity_sync:
@@ -29,10 +33,30 @@ func _process(_delta: float) -> void:
 	elif velocity_sync.x > 0:
 		$AnimatedSprite2D.flip_h = false
 
-func move(direction: Vector2):
+
+func process_input():
+	if player_id != multiplayer.get_unique_id():
+		return
+
+	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if not multiplayer.is_server():
+		_move(direction)
+
+	_request_move.rpc(direction)
+
+
+func _move(direction: Vector2):
 	velocity.x = direction.x * SPEED
 	velocity.y = direction.y * SPEED
 	
 	velocity_sync = velocity
 		
 	move_and_slide()
+
+
+@rpc("any_peer", "call_local")
+func _request_move(direction: Vector2):
+	if not multiplayer.is_server():
+		return
+	
+	_move(direction)
--- Game/game_root.gd
+++ Game/game_root.gd
@@ -6,10 +6,6 @@ func _ready() -> void:
 		_spawn_characters()
 
 
-func _process(_delta) -> void:
-	move()
-
-
 func _spawn_characters():
 	var i := 1
 	for peer_id in LobbyManager.get_all_peer_ids():
@@ -28,44 +24,3 @@ func _spawn_character(spawn_position: Vector2, player_id: int) -> void:
 	character.color = peer_config["player_color"]
 
 	$PlayerSpawner.add_child(character, true)
-
-
-func move():
-	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-
-	if not multiplayer.is_server():
-		var player_character = _find_player_character(multiplayer.get_unique_id())
-		if player_character:
-			player_character.move(direction)
-
-	_request_move.rpc(direction)
-
-
-@rpc("any_peer", "call_local")
-func _request_move(direction: Vector2):
-	if not multiplayer.is_server():
-		return
-
-	var player_character = _find_player_character(
-		multiplayer.get_remote_sender_id())
-	
-	player_character.move(direction)
-
-
-func _find_player_character(player_id):
-	var player_character : CharacterBody2D
-	for node in $PlayerSpawner.get_children():
-		if not node is CharacterBody2D:
-			continue
-		
-		if not node.player_id == player_id:
-			continue
-		
-		player_character = node
-	
-	if not player_character:
-		push_warning("Tried to move character that doesn't exist")
-		return null
-
-	return player_character
-
