005cead4e8d8891e2578b4297e332dd39cb5e143
Ronald Leppink <rleppink@Ronalds-MacBook-Air.local>
2023-10-24 10:48:26 +0200

    Use custom spawn function
    This is so that the clients also get updated values on their player
    nodes

--- Game/game_root.gd
+++ Game/game_root.gd
@@ -1,56 +1,40 @@
 extends Node2D
 
 
-var local_player : Player
-
-
 func _ready() -> void:
+	%PlayerSpawner.spawn_function = _spawn_character
+
 	if multiplayer.is_server():
 		_spawn_characters()
 
 
-func _process(_delta):
-	_highlight_player_target()
-
-
-func _highlight_player_target():
-	var player_local_position : Vector2i = %TileMap.to_local(local_player.center)
-	var cell : Vector2i = %TileMap.local_to_map(player_local_position)
-	var target_cell := cell + local_player.target_direction
-	var cell_local_position : Vector2 = %TileMap.map_to_local(target_cell)
-	$Target.position = %TileMap.to_global(cell_local_position)
-
-
-func _tween_target():
-	var tween := create_tween()
-	tween.set_loops()
-	tween.tween_property($Target, "scale", Vector2(-0.5, -0.5), 0.16).as_relative()
-	tween.tween_property($Target, "scale", Vector2(0.5, 0.5), 0.16).as_relative()
-
-
-func _spawn_characters():
+func _spawn_characters() -> void:
 	var i := 0
 	for peer_id in LobbyManager.get_all_peer_ids():
-		var character := _spawn_character(
-				%PlayerSpawnPositions.get_children()[i].position,
-				peer_id)
-		
-		if peer_id ==  multiplayer.get_unique_id():
-			local_player = character
+		var player_spawn_data := _build_spawn_data(peer_id, i)
+		%PlayerSpawner.spawn(player_spawn_data)
 
 		i += 1
 
 
-func _spawn_character(spawn_position: Vector2, player_id: int) -> Player:
+func _spawn_character(spawn_data: Dictionary) -> Player:
 	var character = preload("res://Game/player.tscn").instantiate()
-	character.position = spawn_position
-	character.name = "Character_%s" % str(player_id)
-
-	character.player_id = player_id
-	var peer_config = LobbyManager.get_peer_config(player_id)
-	character.player_name = peer_config["player_name"]
-	character.color = peer_config["player_color"]
-
-	$PlayerSpawner.add_child(character, true)
+	character.position = spawn_data.spawn_position
+	character.name = "Character_%s" % str(spawn_data.id)
+	character.player_id = spawn_data.id
+	character.player_name = spawn_data.name
+	character.color = spawn_data.color
 
 	return character
+
+
+func _build_spawn_data(player_id: int, spawn_position_index: int) -> Dictionary:
+	var peer_config := LobbyManager.get_peer_config(player_id)
+	var spawn_data := {
+		"id": player_id,
+		"spawn_position": %PlayerSpawnPositions.get_children()[spawn_position_index].position,
+		"name": peer_config["player_name"],
+		"color": peer_config["player_color"],
+	}
+
+	return spawn_data
