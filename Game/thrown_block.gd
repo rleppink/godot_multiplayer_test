155cdd2d402ddc7a2504b5cb4da7645d551e336f
Ronald Leppink <rleppink@Ronalds-MacBook-Air.local>
2023-10-29 16:16:54 +0100

    Modulate throw time and offset by distance

--- Game/thrown_block.gd
+++ Game/thrown_block.gd
@@ -6,19 +6,23 @@ var tween_finished: Callable = func(): push_error("Tween finished not initialize
 
 
 func _ready():
+	var throw_distance := position.distance_to(target_position)
+	var throw_time := throw_distance / 750
+	var throw_offset := throw_distance / 15
+
 	var offset_tween := create_tween()
-	offset_tween.tween_property($Sprite2D, "offset", Vector2(0, -30), 0.1) \
+	offset_tween.tween_property($Sprite2D, "offset", Vector2(0, -throw_offset), throw_time * 0.2) \
 			.as_relative() \
 			.set_ease(Tween.EASE_OUT) \
 			.set_trans(Tween.TRANS_QUINT)
 
-	offset_tween.tween_property($Sprite2D, "offset", Vector2(0, 30), 0.4) \
+	offset_tween.tween_property($Sprite2D, "offset", Vector2(0, throw_offset), throw_time * 0.8) \
 			.as_relative() \
 			.set_ease(Tween.EASE_OUT) \
 			.set_trans(Tween.TRANS_QUINT)
 
 	var position_tweener := \
-			create_tween().tween_property(self, "position", target_position, 0.5) \
+			create_tween().tween_property(self, "position", target_position, throw_time) \
 				.set_ease(Tween.EASE_OUT) \
 				.set_trans(Tween.TRANS_QUINT)
 
